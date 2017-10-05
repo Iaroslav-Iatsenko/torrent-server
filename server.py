6c8b669be31326aa46969ba69e6dea8bf2a74b5c
Mykyta Pashura <mpashura@mpashura.local>
2017-10-06 00:20:01 +0300

    refactoring

--- server.py
+++ server.py
@@ -1,63 +1,43 @@
 import asyncio
-import logging
-
-log = logging.getLogger(__name__)
-
-clients = {}  # task -> (reader, writer)
 
 
 def server():
     loop = asyncio.get_event_loop()
-    f = asyncio.start_server(accept_client, host=None, port=2991)
-    loop.run_until_complete(f)
-    loop.run_forever()
+    f = asyncio.start_server(handle_client, host='127.0.0.1', port=2991, loop=loop)
+    run_server = loop.run_until_complete(f)
 
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(run_server.sockets[0].getsockname()))
 
-def accept_client(client_reader, client_writer):
-    task = asyncio.Task(handle_client(client_reader, client_writer))
-    clients[task] = (client_reader, client_writer)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-    def client_done(client_task):
-        del clients[client_task]
-        client_writer.close()
-        log.warning("End Connection")
-
-    log.warning("New Connection")
-    task.add_done_callback(client_done)
+    # Close the server
+    run_server.close()
+    loop.run_until_complete(run_server.wait_closed())
+    loop.close()
 
 
 @asyncio.coroutine
 def handle_client(client_reader, client_writer):
     # let the client know they are connected
-    client_writer.write("Connection established\n".encode())
+    print("Send: Connection established")
+    client_writer.write("Connection established".encode())
 
     # give client a chance to respond, timeout after 10 seconds
-    data = yield from asyncio.wait_for(client_reader.readline(),
-                                       timeout=10.0)
+    data = yield from asyncio.wait_for(client_reader.read(1000), timeout=10.0)
 
-    if data is None:
-        log.warning("Expected some data, received None")
-        return
+    message = data.decode()
+    address = client_writer.get_extra_info('peername')
+    print("Received {} from {}".format(message, address))
 
-    some_message = data.decode().rstrip()
-    log.warning("Received {}".format(some_message))
+    # schedule the write operation and flush the buffer
+    yield from client_writer.drain()
 
-    client_writer.write("READY\n".encode())
-    while True:
-        # wait for input from client
-        data = yield from asyncio.wait_for(client_reader.readline(),
-                                           timeout=10.0)
-        if data is None:
-            log.warning("Received no data")
-            # exit echo loop and disconnect
-            return
-
-        sent_data = data.decode().rstrip()
-        if sent_data.upper() == 'BYE':
-            client_writer.write("BYE\n".encode())
-            break
-        response = ("ECHO {}\n".format(sent_data))
-        client_writer.write(response.encode())
+    print("Close the client socket")
+    client_writer.close()
 
 
 if __name__ == '__main__':
