9971c4156a2b6e5a6ae77c170dcca46374d7c608
Iaroslav-Iatsenko <yaryat@ua.fm>
2017-10-13 08:33:56 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 import asyncio
-import os
-import math
 
 @asyncio.coroutine
 def handle_client(client_reader, client_writer):
@@ -48,30 +46,6 @@ class SimpleTorrentServer:
         self.run_server.close()
         self.loop.run_until_complete(self.run_server.wait_closed())
         self.loop.close()
-        
-class Shard:
-    def __init__(self, filename, index, open=open):
-        self._index = index
-        with open(filename, 'rb') as file:
-            file.seek(index * SPLIT_STEP)
-            self._data = file.read(SPLIT_STEP)
-
-    def save(self, filename):
-        with open(filename, 'wb') as file:
-            file.seek(self._index * SPLIT_STEP)
-file.write(self._data)        
-
-SPLIT_STEP = 1024
-def file_lenth(file):
-    return os.stat('somefile.txt').st_size
-
-def filespliter(self, file):
-    self.file=file
-    self.filelenth= file_lenth(self,file)
-    self.total_shards=math.ceil(self.filelenth/SPLIT_STEP)
-    self.sherds_list={}
-    for i in range(self.total_shards):
-        self.sherds_list[i] = Shard(self.file, i)
         
 
     
